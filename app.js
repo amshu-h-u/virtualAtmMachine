1dbd05b6eda44a69987995faa3afdce1dc438821
Amshu H U <amshugowda177@gmail.com>
2026-04-29 16:59:06 +0530

    Template created

---
+++ app.js
@@ -0,0 +1,15 @@
+document.body.style.padding="0%"
+document.body.style.margin="0%"
+document.body.style.backgroundColor="black"
+
+let h1=document.getElementsByTagName("h1")[0]
+h1.innerText="ATM Machine"
+h1.style.color="white"
+h1.style.fontFamily="sans-serif"
+h1.style.textAlign="center"
+
+let container=document.getElementsByTagName("div")[0]
+container.style.height="200px"
+container.style.width="500px"
+container.style.border="1px solid black"
+container.style.borderRadius="3px"
